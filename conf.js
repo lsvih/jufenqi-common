5ac896d20c333a06523bdcb3740b78440f72a4b2
lyz <408772145@qq.com>
2016-11-29 14:44:31 +0800

    Change to new server

--- conf.js
+++ conf.js
@@ -1,17 +1,18 @@
 
+var server = `http://wx.jufenqi.com/`
 var Rxports = {
-  appname: '居分期',
-  server: 'http://wangyb.net:8080/',
-  userApi: 'http://wangyb.net:8080/user/api/',
-  walletApi: 'http://wangyb.net:8080/wallet/api/',
-  orderApi: 'http://wangyb.net:8080/decorationorder/api/',
-  mOrderApi: 'http://wangyb.net:8080/materialorder/api/',
-  merApi: 'http://wangyb.net:8080/merchant/api/',
-  loanApi: 'http://wangyb.net:8080/loanapplicant/api/',
-  wxApi:'http://wangyb.net:8080/wechat/api/',
-  serverUrl: 'http://101.200.78.103:90/',
-  apiUrl: 'http://101.200.78.103:90/jfq/api/web/',
-  imgUrl: 'http://oexqgmcri.bkt.clouddn.com/'
+  appname: `居分期`,
+  server: server,
+  userApi: `${server}user/api/`,
+  walletApi: `${server}wallet/api/`,
+  orderApi: `${server}decorationorder/api/`,
+  mOrderApi: `${server}materialorder/api/`,
+  merApi: `${server}merchant/api/`,
+  loanApi: `${server}loanapplicant/api/`,
+  wxApi:`${server}wechat/api/`,
+  serverUrl: `http://101.200.78.103:90/`,
+  apiUrl: `http://101.200.78.103:90/jfq/api/web/`,
+  imgUrl: `http://oexqgmcri.bkt.clouddn.com/`
 }
 
 module.exports = Rxports
