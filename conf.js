55f6b4a9a2f8a3bbc1234678f90f1292226453ea
lyz <408772145@qq.com>
2016-11-30 15:40:10 +0800

    Fixed

--- conf.js
+++ conf.js
@@ -1,18 +1,17 @@
-
 var server = 'http://wx.jufenqi.com:8080/'
 var Rxports = {
-  appname: '居分期',
-  server: server,
-  userApi: `${server}user/api/`,
-  walletApi: `${server}wallet/api/`,
-  orderApi: `${server}decorationorder/api/`,
-  mOrderApi: `${server}materialorder/api/`,
-  merApi: `${server}merchant/api/`,
-  loanApi: `${server}loanapplicant/api/`,
-  wxApi:`${server}wechat/api/`,
-  serverUrl: 'http://101.200.78.103:90/',
-  apiUrl: 'http://101.200.78.103:90/jfq/api/web/',
-  imgUrl: 'http://oexqgmcri.bkt.clouddn.com/'
+    appname: '居分期',
+    server: server,
+    userApi: server + 'user/api/',
+    walletApi: server + 'wallet/api/',
+    orderApi: server + 'decorationorder/api/',
+    mOrderApi: server + 'materialorder/api/',
+    merApi: server + 'merchant/api/',
+    loanApi: server + 'loanapplicant/api/',
+    wxApi: server + 'wechat/api/',
+    serverUrl: 'http://101.200.78.103:90/',
+    apiUrl: 'http://101.200.78.103:90/jfq/api/web/',
+    imgUrl: 'http://oexqgmcri.bkt.clouddn.com/'
 }
 
 module.exports = Rxports
