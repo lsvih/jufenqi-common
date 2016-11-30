8a056f7ed3ae5a15a92653b952076e887006f61c
lyz <408772145@qq.com>
2016-11-30 15:32:50 +0800

    Fixed

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 
-var server = `http://wx.jufenqi.com:8080/`
+var server = 'http://wx.jufenqi.com:8080/'
 var Rxports = {
-  appname: `居分期`,
+  appname: '居分期',
   server: server,
   userApi: `${server}user/api/`,
   walletApi: `${server}wallet/api/`,
@@ -10,9 +10,9 @@ var Rxports = {
   merApi: `${server}merchant/api/`,
   loanApi: `${server}loanapplicant/api/`,
   wxApi:`${server}wechat/api/`,
-  serverUrl: `http://101.200.78.103:90/`,
-  apiUrl: `http://101.200.78.103:90/jfq/api/web/`,
-  imgUrl: `http://oexqgmcri.bkt.clouddn.com/`
+  serverUrl: 'http://101.200.78.103:90/',
+  apiUrl: 'http://101.200.78.103:90/jfq/api/web/',
+  imgUrl: 'http://oexqgmcri.bkt.clouddn.com/'
 }
 
 module.exports = Rxports
